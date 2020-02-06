b27973c3bbf19c3c9e1f21c04ce60ef44006ae59
Fan Wang <wangfanbsg75@live.com>
2020-02-06 09:04:26 -0600

    birthweight data

---
+++ R/ffd_opt_birthwt.R
@@ -0,0 +1,20 @@
+#' Test Dataset Birth Weight
+#'
+#' Dataset from Baystate Medical Center in Springfield, Massachusetts (Hosmer et. al. 1998)
+#'
+#' @docType data
+#'
+#' @usage data(df_opt_birthwt)
+#'
+#' @format csv
+#'
+#' @keywords datasets
+#'
+#' @references HOSMER, D.W., HOSMER, T., LE CESSIE, S. and LEMESHOW, S. (1997), A COMPARISON OF GOODNESS‐OF‐FIT TESTS FOR THE LOGISTIC REGRESSION MODEL. Statist. Med., 16: 965-980. doi:10.1002/(SICI)1097-0258(19970515)16:9<965::AID-SIM509>3.0.CO;2-O
+#'
+#' @source \url{https://onlinelibrary.wiley.com/doi/book/10.1002/9781118548387}
+#'
+#' @examples
+#' data(df_opt_birthwt)
+#' head(df_opt_birthwt, 10)
+"df_opt_birthwt"
--- R/ffd_opt_dtgch_aorig.R
+++ R/ffd_opt_dtgch_aorig.R
@@ -11,7 +11,7 @@
 #'
 #' @keywords datasets
 #'
-#' @references “Early life height and weight production functions with endogenous energy and protein inputs”, September 2016, 65-81, Economics & Human Biology, 22, Esteban Puentes, Fan Wang, Jere R. Behrman, Flavio Cunha, John Hoddinott, John A. Maluccio, Linda S. Adair, Judith Borja and Reynaldo Martorell
+#' @references "Early life height and weight production functions with endogenous energy and protein inputs", September 2016, 65-81, Economics & Human Biology, 22, Esteban Puentes, Fan Wang, Jere R. Behrman, Flavio Cunha, John Hoddinott, John A. Maluccio, Linda S. Adair, Judith Borja and Reynaldo Martorell
 #'
 #' @source \url{https://www.sciencedirect.com/science/article/pii/S1570677X16300107}
 #'
@@ -19,5 +19,5 @@
 #' data(df_opt_dtgch_aorig)
 #' var_wgt <- df_opt_dtgch_aorig$wgt
 #' head(df_opt_dtgch_aorig, 10)
-#' var_wgt[1:10] 
+#' var_wgt[1:10]
 "df_opt_dtgch_aorig"
